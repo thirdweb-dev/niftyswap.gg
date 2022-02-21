ed1e93e20c251700e1187b2ec552c21bb7e5f88b
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-02-21 12:54:23 -0700

    nuke

--- contracts/testing/CoolCats.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL 3.0
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
-
-contract CoolCats is ERC721PresetMinterPauserAutoId {
-    constructor() ERC721PresetMinterPauserAutoId("Cool Cats", "COOL", "https://api.coolcatsnft.com/cat/") {}
-}
--- contracts/testing/Punks.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL 3.0
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
-
-contract Punks is ERC721PresetMinterPauserAutoId {
-    constructor() ERC721PresetMinterPauserAutoId("Punks", "PUNK", "ipfs://dummy") {}
-}
